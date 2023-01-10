e6eef021f661f88bfc8631382ff6eae55b3d63e3
desplega <desplega@desplega.com>
2023-01-10 12:13:15 +0100

    First version

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Upload list') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form method="POST" action={{ route('dashboard') }} enctype="multipart/form-data">
+                        <div class="mt-4">
+                            <x-input-label for="csvlist" :value="__('CSV List')" />
+
+                            <input class="mt-1 w-full" name="csvlist" type="file" id="csvlist">
+                            <x-input-error :messages="$errors->get('csvlist')" class="mt-2" />
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <x-primary-button type="reset" class="ml-3">
+                                {{ __('Reset') }}
+                            </x-primary-button>
+                            <x-primary-button class="ml-3">
+                                {{ __('Upload') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
